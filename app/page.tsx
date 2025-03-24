149d3caceab153ccd51db00ba27a7c7a7ab7e2d3
Valentin Stockman <valentin.stockman.ext@planity.com>
2025-03-24 11:22:13 +0100

    chore: modify VStockman logo and remove triangle shape from login page

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,6 @@
 import VStockmanLogo from '@/app/ui/vstockman-logo';
 import { ArrowRightIcon } from '@heroicons/react/24/outline';
 import Link from 'next/link';
-import styles from '@/app/ui/home.module.css';
 import { lusitana } from './ui/fonts';
 import Image from 'next/image';
 
@@ -13,7 +12,6 @@ export default function Page() {
       </div>
       <div className='mt-4 flex grow flex-col gap-4 md:flex-row'>
         <div className='flex flex-col justify-center gap-6 rounded-lg bg-gray-50 px-6 py-10 md:w-2/5 md:px-20'>
-          <div className={styles.shape} />
           <p
             className={`${lusitana.className} text-xl text-gray-800 md:text-3xl md:leading-normal`}
           >
@@ -38,14 +36,14 @@ export default function Page() {
             width={1000}
             height={760}
             className='hidden md:block'
-            alt='Screenshots of the dashboard project showing desktop version'
+            alt='Picture of VStockman standing in front of the camera'
           />
           <Image
             src='/v-stockman.png'
             width={560}
             height={620}
             className='block md:hidden'
-            alt='Screenshots of the dashboard project showing mobile version'
+            alt='Picture of VStockman standing in front of the camera'
           />
         </div>
       </div>
--- app/ui/vstockman-logo.tsx
+++ app/ui/vstockman-logo.tsx
@@ -1,4 +1,4 @@
-import { GlobeAltIcon } from '@heroicons/react/24/outline';
+import { BanknotesIcon } from '@heroicons/react/24/outline';
 import { lusitana } from '@/app/ui/fonts';
 
 export default function VStockmanLogo() {
@@ -6,7 +6,7 @@ export default function VStockmanLogo() {
     <div
       className={`${lusitana.className} flex flex-row items-center leading-none text-white`}
     >
-      <GlobeAltIcon className='h-12 w-12 rotate-[15deg]' />
+      <BanknotesIcon className='h-12 w-12 rotate-[15deg]' />
       <p className='text-[44px]'>VStockman</p>
     </div>
   );
